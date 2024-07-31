dbdf54858e3ad7554bab0f28f1b559a3b3e110d8
Rafiyad Darazat <141349190+rafiyad@users.noreply.github.com>
2024-07-31 17:09:28 +0600

    Add files via upload

---
+++ Day-10.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        // write codes here
+        int counter=0;
+        int max=0;
+        while(n>0){
+            int a =n%2;
+            if(a==1){
+                counter++;
+                if(counter>max){
+                    max=counter;
+                }
+            }else{
+                counter=0;
+            }   
+            n=n/2;
+        }
+        System.out.println(max);
+        
+        
+        bufferedReader.close();
+    }
+}
